f94e474f6f59e1e10af0d3cc6f96125a6fe4d281
Dat <e29bechanh@gmail.com>
2022-10-17 20:02:49 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          chị khoai ăn cứt 
         </a>
       </header>
     </div>
